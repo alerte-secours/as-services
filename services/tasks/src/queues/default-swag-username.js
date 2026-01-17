07f23be096126855c9413a936d6a4af307decfd7
devthejo <devthejo@protonmail.com>
2026-01-17 13:59:48 +0100

    fix: swag username

--- services/tasks/src/queues/default-swag-username.js
+++ services/tasks/src/queues/default-swag-username.js
@@ -15,7 +15,7 @@ module.exports = async function () {
       await updateUserWithUniqueUsername({ userId })
     },
     {
-      dedupOptions: { enabled: false },
+      dedupOptions: { enabled: true },
     }
   )
 }
